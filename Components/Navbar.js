ca983430955ed91ec7fe7e70d906b270635f6e64
cyberkillua <moshood988@gmail.com>
2023-06-04 20:41:19 +0100

    header done

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -2,17 +2,18 @@ import React from "react";
 import styles from "../styles/navbar/navbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import btn from "../public/btn.svg";
 
 const Navbar = () => {
   return (
     <nav className={styles.nav} id="top">
       <div>
-        <Link href="mailto:moshood988@gmail.com">
-          <a target="_blank" rel="noopener noreferrer">
-            <button>
-              <Image src={btn} alt="btn" />
-            </button>
-          </a>
+        <Link
+          href="mailto:moshood988@gmail.com"
+          arget="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className={styles.btn}>Contact Me</button>
         </Link>
       </div>
     </nav>
