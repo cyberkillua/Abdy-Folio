7ae06b1d8689da34e6f84bc34e10400b94f41ebb
cyberkillua <moshood988@gmail.com>
2023-06-24 16:31:37 +0100

    pictures fix

--- Components/Project.js
+++ Components/Project.js
@@ -2,10 +2,10 @@ import React from "react";
 import styles from "../styles/project/project.module.css";
 // import { gsap } from "gsap";
 // import { useEffect } from "react";
-import sunday from "../public/Sunday-Ball.webp";
-import cyber from "../public/CyberKillua.webp";
-import aidoo from "../public/Aidoo.webp";
-import generator from "../public/Generator.webp";
+import sunday from "../public/Sunday-Ball.svg";
+import cyber from "../public/CyberKillua.png";
+import aidoo from "../public/Aidoo.svg";
+import generator from "../public/Generator.svg";
 
 import Image from "next/image";
 // import Link from "next/link";
